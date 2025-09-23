1ea0549465c16ad617705e2c4a5bf99a09b2f6b5
martim <1181754@isep.ipp.pt>
2025-09-23 22:27:47 +0100

    Database changed

--- CA1/spring-framework-petclinic/src/main/resources/db/h2/data.sql
+++ CA1/spring-framework-petclinic/src/main/resources/db/h2/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO vets VALUES (default, 'James', 'Carter');
-INSERT INTO vets VALUES (default, 'Helen', 'Leary');
-INSERT INTO vets VALUES (default, 'Linda', 'Douglas');
-INSERT INTO vets VALUES (default, 'Rafael', 'Ortega');
-INSERT INTO vets VALUES (default, 'Henry', 'Stevens');
-INSERT INTO vets VALUES (default, 'Sharon', 'Jenkins');
+INSERT INTO vets VALUES (default, 'James', 'Carter','574845');
+INSERT INTO vets VALUES (default, 'Helen', 'Leary','574845555');
+INSERT INTO vets VALUES (default, 'Linda', 'Douglas','574432432845');
+INSERT INTO vets VALUES (default, 'Rafael', 'Ortega','57484542342342');
+INSERT INTO vets VALUES (default, 'Henry', 'Stevens','5748456565');
+INSERT INTO vets VALUES (default, 'Sharon', 'Jenkins','574845ASD');
 
 INSERT INTO specialties VALUES (default, 'radiology');
 INSERT INTO specialties VALUES (default, 'surgery');
